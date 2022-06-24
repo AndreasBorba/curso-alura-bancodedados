c84d656d7cbf8b0d4cda2c5fccfecc07c9c47c06
AndreasBorba <borba_andreas@hotmail.com>
2022-06-23 21:57:45 -0300

    Cursor, Loop e Exceções de Erro

---
+++ PLSQL Procedure, Funções e Exceções/cursor.sql
@@ -0,0 +1,16 @@
+DECLARE
+    CURSOR cur_CLIENTE IS SELECT ID, RAZAO_SOCIAL FROM TAB_CLIENTE;
+    v_ID TAB_CLIENTE.ID%TYPE;
+    v_RAZAO_SOCIAL TAB_CLIENTE.RAZAO_SOCIAL%TYPE;
+BEGIN
+    OPEN cur_CLIENTE;
+    LOOP
+        FETCH cur_CLIENTE INTO v_ID, v_RAZAO_SOCIAL;
+    EXIT WHEN cur_CLIENTE%NOTFOUND;
+        dbms_output.put_line('ID: ' || v_ID);
+        dbms_output.put_line('Razão Social: ' || v_RAZAO_SOCIAL);
+    END LOOP;
+    CLOSE cur_CLIENTE;
+END;
+
+SELECT * FROM TAB_CLIENTE;
